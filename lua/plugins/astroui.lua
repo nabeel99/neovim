e70ba0ae5c2901beab60a2065e0edf756b2236e4
oyelowo <oyelowooyedayo@gmail.com>
2024-04-02 10:50:25 -0600

    Fix user lua

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -9,8 +9,8 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    -- colorscheme = "astrodark",
-    colorscheme = "vim-code-dark",
+    colorscheme = "astrodark",
+    -- colorscheme = "vim-code-dark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
