32efb30a68a1506dd9d0543b2c39fd45fd1b6789
oyelowo <oyelowooyedayo@gmail.com>
2024-06-29 10:00:40 +0300

    add custom commands

---
+++ lua/mappings/rust.lua
@@ -0,0 +1,28 @@
+local rust_cmds = {
+    name = "Rust Commands",
+    d = {":RustLsp debuggables<CR>", 'List debug targets'},
+    D = {":RustLsp debug<CR>", 'Start debug session'},
+    r = {":RustLsp runnables<CR>", 'List run targets'},
+    R = {":RustLsp run<CR>", 'Run target'},
+    t = {":RustLsp testables<CR>", 'List test targets'},
+    T = {":RustLsp test<CR>", 'Run tests'},
+    k = {":RustLsp expandMacro<CR>", 'Expand macros recursively'},
+    p = {":RustLsp rebuildProcMacros<CR>", 'Rebuild procedural macros'},
+    u = {":RustLsp moveItem up<CR>", 'Move item up'},
+    n = {":RustLsp moveItem down<CR>", 'Move item down'},
+    c = {":RustLsp codeAction<CR>", 'Code actions'},
+    h = {":RustLsp hover actions<CR>", 'Hover actions'},
+    e = {":RustLsp explainError<CR>", 'Explain error'},
+    C = {":RustLsp openCargo<CR>", 'Open Cargo.toml'},
+    o = {":RustLsp openDocs<CR>", 'Open online docs'},
+    P = {":RustLsp parentModule<CR>", 'Parent module'},
+    j = {":RustLsp joinLines<CR>", 'Join lines'},
+    s = {":RustLsp ssr<CR>", 'Structural search replace'},
+    g = {":RustLsp crateGraph<CR>", 'Crate graph'},
+    S = {":RustLsp syntaxTree<CR>", 'Syntax tree'},
+    f = {":RustLsp flyCheck<CR>", 'Fly check'},
+    v = {":RustLsp view hir<CR>", 'View HIR'},
+    V = {":RustLsp view mir<CR>", 'View MIR'},
+}
+
+return rust_cmds
