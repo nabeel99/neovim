8daba8ce36b29e865897a8aa5fead0cf16290757
oyelowo <oyelowooyedayo@gmail.com>
2024-07-03 02:24:35 +0300

    Add new telescropt goto type def and new rust mappings

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -6,8 +6,8 @@ local telescope_cmds = {
   h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
   -- LSP related
   r = { "<cmd>Telescope lsp_references<cr>", "Search References" },
-  t = { "<cmd>Telescope lsp_references<cr>", "Search References" },
   d = { "<cmd>Telescope lsp_definitions<cr>", "Go to Definitions" },
+  t = { "<cmd>Telescope lsp_type_definitions<cr>", "Definition of current type" },
   i = { "<cmd>Telescope lsp_implementations<cr>", "Go to Implementations" },
   s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
 }
@@ -17,6 +17,7 @@ local opts = { noremap = true, silent = true }
 -- local telescope_cmds = require('plugins.telescope_commands')
 -- local telescope_cmds = require('mappings.telescope')
 map('n', 'grr', "<cmd>Telescope lsp_references<cr>", opts)  -- Added mapping for grr
+map('n', 'grt', "<cmd>Telescope lsp_type_definitions<cr>", opts)  -- Added mapping for gt
 -- map('n', 'grr', telescope_cmds.lr[1], opts)  -- Added mapping for grr
 
 return telescope_cmds
