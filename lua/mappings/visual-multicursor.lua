890fd99265472b39096d0832fc232ed2ce97060d
oyelowo <oyelowooyedayo@gmail.com>
2024-07-13 18:59:41 +0300

    Make the mappings like it was before

--- lua/mappings/visual-multicursor.lua
+++ lua/mappings/visual-multicursor.lua
@@ -1,19 +1,19 @@
 local wk = require("which-key")
 wk.add({
-  { "<leader>kv", group = "visual_multi_cmds" }, -- group for visual multi commands
-  { "<leader>kvm", "<Plug>(VM-Find-Under)", desc = "Find Under Cursor" },
-  { "<leader>kvM", "<Plug>(VM-Find-Subword-Under)", desc = "Find Subword Under Cursor" },
-  { "<leader>kva", "<Plug>(VM-Select-All)", desc = "Select All Occurrences" },
-  { "<leader>kvh", "<Plug>(VM-Select-h)", desc = "Select h" },
-  { "<leader>kvl", "<Plug>(VM-Select-l)", desc = "Select l" },
-  { "<leader>kvj", "<Plug>(VM-Select-j)", desc = "Select j" },
-  { "<leader>kvk", "<Plug>(VM-Select-k)", desc = "Select k" },
-  { "<leader>kvn", "<Plug>(VM-Next)", desc = "Select Next" },
-  { "<leader>kvN", "<Plug>(VM-Prev)", desc = "Select Previous" },
-  { "<leader>kvq", "<Plug>(VM-Skip)", desc = "Skip Current" },
-  { "<leader>kvQ", "<Plug>(VM-Remove)", desc = "Remove Current" },
-  { "<leader>kvi", "<Plug>(VM-Insert)", desc = "Start Insert Mode" },
-  { "<leader>kvA", "<Plug>(VM-Append)", desc = "Start Append Mode" },
+  { "<leader>km", group = "visual_multi_cmds" }, -- group for visual multi commands
+  { "<leader>kmm", "<Plug>(VM-Find-Under)", desc = "Find Under Cursor" },
+  { "<leader>kmM", "<Plug>(VM-Find-Subword-Under)", desc = "Find Subword Under Cursor" },
+  { "<leader>kma", "<Plug>(VM-Select-All)", desc = "Select All Occurrences" },
+  { "<leader>kmh", "<Plug>(VM-Select-h)", desc = "Select h" },
+  { "<leader>kml", "<Plug>(VM-Select-l)", desc = "Select l" },
+  { "<leader>kmj", "<Plug>(VM-Select-j)", desc = "Select j" },
+  { "<leader>kmk", "<Plug>(VM-Select-k)", desc = "Select k" },
+  { "<leader>kmn", "<Plug>(VM-Next)", desc = "Select Next" },
+  { "<leader>kmN", "<Plug>(VM-Prev)", desc = "Select Previous" },
+  { "<leader>kmq", "<Plug>(VM-Skip)", desc = "Skip Current" },
+  { "<leader>kmQ", "<Plug>(VM-Remove)", desc = "Remove Current" },
+  { "<leader>kmi", "<Plug>(VM-Insert)", desc = "Start Insert Mode" },
+  { "<leader>kmA", "<Plug>(VM-Append)", desc = "Start Append Mode" },
   { "<leader><D-d>", "<Plug>(VM-Add-Cursor-Down)", desc = "Add Cursor Down" },
   { "<leader><D-u>", "<Plug>(VM-Add-Cursor-Up)", desc = "Add Cursor Up" },
   { "<leader><C-Down>", "<Plug>(VM-Add-Cursor-Down)", desc = "Add Cursor Down" },
@@ -21,28 +21,3 @@ wk.add({
 })
 
 
-
-
-
--- local visual_multi_cmds = {
---   name = "Visual Multi",
---   ["m"] = {"<Plug>(VM-Find-Under)", "Find Under Cursor"},
---   ["M"] = {"<Plug>(VM-Find-Subword-Under)", "Find Subword Under Cursor"},
---   ["a"] = {"<Plug>(VM-Select-All)", "Select All Occurrences"},
---   h = {"<Plug>(VM-Select-h)", "Select h"},
---   l = {"<Plug>(VM-Select-l)", "Select l"},
---   j = {"<Plug>(VM-Select-j)", "Select j"},
---   k = {"<Plug>(VM-Select-k)", "Select k"},
---   ["n"] = {"<Plug>(VM-Next)", "Select Next"},
---   ["N"] = {"<Plug>(VM-Prev)", "Select Previous"},
---   ["q"] = {"<Plug>(VM-Skip)", "Skip Current"},
---   ["Q"] = {"<Plug>(VM-Remove)", "Remove Current"},
---   ["i"] = {"<Plug>(VM-Insert)", "Start Insert Mode"},
---   ["A"] = {"<Plug>(VM-Append)", "Start Append Mode"},
---   ["<D-d>"] = {"<Plug>(VM-Add-Cursor-Down)", "Add Cursor Down"},  -- Cmd+d
---   ["<D-u>"] = {"<Plug>(VM-Add-Cursor-Up)", "Add Cursor Up"},      -- Cmd+u
---   ['<C-Down>'] = {"<Plug>(VM-Add-Cursor-Down)", "Add Cursor Down"},
---   ['<C-Up>'] = {"<Plug>(VM-Add-Cursor-Up)", "Add Cursor Up"},
--- }
---
--- return visual_multi_cmds
