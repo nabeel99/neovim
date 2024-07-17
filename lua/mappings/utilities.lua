1d06c738e6628d2b0a92ff5b7ad231fb89482d62
oyelowo <oyelowooyedayo@gmail.com>
2024-07-17 13:08:40 +0300

    Support opening in macos finder

---
+++ lua/mappings/utilities.lua
@@ -0,0 +1,18 @@
+local wk = require("which-key")
+
+
+vim.api.nvim_create_user_command('Rfinder',
+    function()
+        local path = vim.api.nvim_buf_get_name(0)
+        os.execute('open -R ' .. path)
+    end,
+    {}
+)
+
+
+wk.add({
+  { "<leader>ku", group = "Utility Functions For Common Stuff" }, -- group for Common Utility functions
+  { "<leader>kuo", "<cmd>Rfinder<cr>", desc = "Open In MacOs Finder" },
+})
+
+
