09f7182134534349c48078ec78fd679b9693a530
oyelowo <oyelowooyedayo@gmail.com>
2024-07-24 17:48:24 +0300

    Add new key bidning

--- lua/mappings/pomodoro.lua
+++ lua/mappings/pomodoro.lua
@@ -6,7 +6,8 @@ wk.add({
   { "<leader>kp", group = "Pomodori Timers" }, -- group for timer commands
   { "<leader>kp1", ":TimerStart 5<CR>", desc = "Start a 5 sec1onds new timer" },
   { "<leader>kp0", ":TimerStart 300<CR>", desc = "Start a 5 Minutes new timer" },
-  { "<leader>kp9", ":TimerStart 900<CR>", desc = "Start a 15 Minutes new timer" },
+  { "<leader>kpp", ":TimerStart 900<CR>", desc = "Start a 15 Minutes new timer" },
+  -- { "<leader>kp9", ":TimerStart 900<CR>", desc = "Start a 15 Minutes new timer" },
   -- { "<leader>kp5", ":TimerStart <args><CR>", desc = "Start a new timer" },
   { "<leader>kpT", ":TimerStop <args><CR>", desc = "Stop a running timer" },
   { "<leader>kpR", ":TimerRepeat <args><CR>", desc = "Start a repeat timer" },
