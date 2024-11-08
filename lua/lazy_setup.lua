ac468f2b66e45b2c97afcbb3f1d25feccea768c3
oyelowo <oyelowooyedayo@gmail.com>
2024-11-08 20:04:06 +0200

    Update overrides nd setup

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -8,15 +8,18 @@ require("lazy").setup({
       maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
       icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
       pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+      ui_notifications_enabled = false
     },
   },
   { import = "community" },
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrodark", "habamax", "vim-code-dark" } },
-  ui = { backdrop = 100 },
+  install = { colorscheme = { "vim-code-dark", "astrodark", "habamax" } },
+  ui = { 
+      backdrop = 100, 
+      ui_notifications_enabled = false
+    },
   performance = {
     rtp = {
       -- disable some rtp plugins, add more to your liking
