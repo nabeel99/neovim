6746d4afcafe263f18b635503445fd6bf9212e36
oyelowo <oyelowooyedayo@gmail.com>
2024-12-10 11:25:17 +0200

    Add more info

---
+++ lua/plugins/persisted.lua
@@ -0,0 +1,7 @@
+-- Not sure yet about this plugin. Date 10/11/2024
+-- Lua
+return {
+  "olimorris/persisted.nvim",
+  lazy = false, -- make sure the plugin is always loaded at startup
+  config = true
+}
