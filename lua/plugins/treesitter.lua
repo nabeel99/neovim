5ffba60ce585aa0f608be0c2abb39d2bf74f44d1
oyelowo <oyelowooyedayo@gmail.com>
2025-01-08 19:22:51 +0200

    Update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,10 +11,42 @@ return {
       "vim",
       -- add more arguments for adding more treesitter parsers
     },
-    highlight = {
-      enable = true,
-      -- disable = { "markdown", "html" },
-    },
+    -- highlight = {
+    --   enable = true,
+    --   -- disable = { "markdown", "html" },
+    -- },
+  -- the related part.
+  highlight = {
+    enable = true,
+    language_tree = true,
+    is_supported = function ()
+      if vim.fn.strwidth(vim.fn.getline('.')) > 300
+        or vim.fn.getfsize(vim.fn.expand('%')) > 1024 * 1024 then
+        return false
+      else
+        return true
+      end
+    end
+  },
+  -- some more parts omitted.
   },
 }
 
+
+
+-- require('nvim-treesitter.configs').setup {
+--   -- the related part.
+--   highlight = {
+--     enable = true,
+--     language_tree = true,
+--     is_supported = function ()
+--       if vim.fn.strwidth(vim.fn.getline('.')) > 300
+--         or vim.fn.getfsize(vim.fn.expand('%')) > 1024 * 1024 then
+--         return false
+--       else
+--         return true
+--       end
+--     end
+--   },
+--   -- some more parts omitted.
+-- }
